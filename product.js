575479afa0269f23d9be9dc49dcd44076aeb52cc
Ahmed <S10258532@connect.np.edu.sg>
2023-11-29 09:29:43 +0800

    added more to the available and price filter

--- product.js
+++ product.js
@@ -58,6 +58,43 @@ function displayCart() {
     cartItemsContainer.appendChild(cartItemDiv);
   });
 }
+function sortBy(option) {
+  const productsSection = document.querySelector(".products");
+  const productItems = document.querySelectorAll(".item");
+
+  const sortedItems = Array.from(productItems);
+
+  if (option === "availability") {
+    sortedItems.sort((a, b) => {
+      const availabilityA = a.querySelector(".availability").textContent;
+      const availabilityB = b.querySelector(".availability").textContent;
+
+      // Assuming availability is in the format 'In Stock' or 'Out of Stock'
+      return availabilityA.localeCompare(availabilityB);
+    });
+  } else if (option === "price") {
+    sortedItems.sort((a, b) => {
+      const priceA = parseFloat(a.querySelector(".price").textContent.slice(1));
+      const priceB = parseFloat(b.querySelector(".price").textContent.slice(1));
+
+      return priceA - priceB;
+    });
+  }
+
+  // Clear and re-append sorted items to the container
+  productsSection.innerHTML = "";
+  sortedItems.forEach((item) => {
+    productsSection.appendChild(item);
+  });
+}
+
+// Listen for changes in the select dropdown and trigger sorting
+document.getElementById("sortSelect").addEventListener("change", function () {
+  const selectedOption = this.value;
+  if (selectedOption !== "default") {
+    sortBy(selectedOption);
+  }
+});
 
 // Function to get price based on product name (Sample implementation)
 function getProductPrice(productName) {
